a1813ad89bd3486faeca30d2e68cb456be93fdd1
cmdrumright <cdrumright@protonmail.com>
2025-11-14 13:55:14 -0600

    added functions to fishList for returning each type of holiness

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -1,25 +1,65 @@
 import { database } from './aquariumData.js';
 
-export const fishList = () => {
+const fishHTML = (fish) => {
     // Generate and HTML representaion of each fish
     let fishHTML = '';
 
-    for (const fish of database.fish) {
-        fishHTML += `
-            <article class="fish">
-                <img src="${fish.image}" alt="${fish.name} image" class="fish__image">
-                <div class"fish__details">
-                    <h2 class="fish__name">${fish.name}</h2>
-                    <ul cladd="fish__detail-list">
-                        <li>${fish.species}</li>
-                        <li>${fish.length} cm</li>
-                        <li>found at ${fish.location}</li>
-                        <li>${fish.diet}</li>
-                    </ul>
-                </div>
-            </article>
-        `;
-    }
+    fishHTML += `
+        <article class="fish">
+            <img src="${fish.image}" alt="${fish.name} image" class="fish__image">
+            <div class"fish__details">
+                <h2 class="fish__name">${fish.name}</h2>
+                <ul cladd="fish__detail-list">
+                    <li>${fish.species}</li>
+                    <li>${fish.length} cm</li>
+                    <li>found at ${fish.location}</li>
+                    <li>${fish.diet}</li>
+                </ul>
+            </div>
+        </article>
+    `;
 
     return fishHTML
 };
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = ""
+
+    for (const fish of database.fish) {
+        // if fish.length is devisible by 3
+        if (fish.length % 3 === 0) {
+            holyFish += fishHTML(fish)
+        }
+    }
+
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const holyishFish = ""
+
+    for (const fish of database.fish) {
+        // if fish.length is devisible by 5
+        if (fish.length % 5 === 0) {
+            holyishFish += fishHTML(fish)
+        }
+    }
+
+    return holyishFish
+}
+
+export const regularFish = () => {
+    // Any fish not a multiple of 3 or 5
+    const unholyFish = ""
+
+    for (const fish of database.fish) {
+        // if fish.length is devisible by 5
+        if (!(fish.length % 5 === 0 || fish.length % 3 === 0)) {
+            unholyFish += fishHTML(fish)
+        }
+    }
+
+    return unholyFish
+}
