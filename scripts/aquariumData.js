66c8e029c2e7d213306a487e2441bacfdf99d7bd
cmdrumright <cdrumright@protonmail.com>
2025-11-14 13:28:29 -0600

    added fish to aquariumData from claude

--- scripts/aquariumData.js
+++ scripts/aquariumData.js
@@ -1,21 +1,125 @@
 export const database = {
     fish: [
-        {
-            name: "Nemo",
-            species: "Clownfish",
-            length: 3,
-            location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "https://example.com/images/nemo.jpg"
-        },
-        {
-            name: "Dory",
-            species: "Blue Tang",
-            length: 5,
-            location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "https://example.com/images/dory.jpg"
-        },
+      {
+        name: "Dory",
+        species: "Blue Tang",
+        length: 2,
+        location: "Great Barrier Reef",
+        diet: "Omnivore",
+        image: "https://example.com/images/dory.jpg"
+      },
+      {
+        name: "Nemo",
+        species: "Clownfish",
+        length: 3,
+        location: "Great Barrier Reef",
+        diet: "Omnivore",
+        image: "https://example.com/images/nemo.jpg"
+      },
+      {
+        name: "Bruce",
+        species: "Great White Shark",
+        length: 15,
+        location: "Pacific Ocean",
+        diet: "Carnivore",
+        image: "https://example.com/images/bruce.jpg"
+      },
+      {
+        name: "Gill",
+        species: "Moorish Idol",
+        length: 6,
+        location: "Indo-Pacific",
+        diet: "Omnivore",
+        image: "https://example.com/images/gill.jpg"
+      },
+      {
+        name: "Bubbles",
+        species: "Yellow Tang",
+        length: 5,
+        location: "Hawaiian Reefs",
+        diet: "Herbivore",
+        image: "https://example.com/images/bubbles.jpg"
+      },
+      {
+        name: "Peach",
+        species: "Starfish",
+        length: 9,
+        location: "Coastal Waters",
+        diet: "Carnivore",
+        image: "https://example.com/images/peach.jpg"
+      },
+      {
+        name: "Jacques",
+        species: "Pacific Cleaner Shrimp",
+        length: 4,
+        location: "Coral Reefs",
+        diet: "Omnivore",
+        image: "https://example.com/images/jacques.jpg"
+      },
+      {
+        name: "Bloat",
+        species: "Porcupine Pufferfish",
+        length: 12,
+        location: "Tropical Reefs",
+        diet: "Carnivore",
+        image: "https://example.com/images/bloat.jpg"
+      },
+      {
+        name: "Anchor",
+        species: "Hammerhead Shark",
+        length: 10,
+        location: "Warm Ocean Waters",
+        diet: "Carnivore",
+        image: "https://example.com/images/anchor.jpg"
+      },
+      {
+        name: "Squirt",
+        species: "Sea Turtle",
+        length: 18,
+        location: "East Australian Current",
+        diet: "Omnivore",
+        image: "https://example.com/images/squirt.jpg"
+      },
+      {
+        name: "Marlin",
+        species: "Clownfish",
+        length: 7,
+        location: "Great Barrier Reef",
+        diet: "Omnivore",
+        image: "https://example.com/images/marlin.jpg"
+      },
+      {
+        name: "Crush",
+        species: "Green Sea Turtle",
+        length: 20,
+        location: "Ocean Currents",
+        diet: "Herbivore",
+        image: "https://example.com/images/crush.jpg"
+      },
+      {
+        name: "Nigel",
+        species: "Pelican",
+        length: 8,
+        location: "Sydney Harbor",
+        diet: "Carnivore",
+        image: "https://example.com/images/nigel.jpg"
+      },
+      {
+        name: "Destiny",
+        species: "Whale Shark",
+        length: 21,
+        location: "Open Ocean",
+        diet: "Filter Feeder",
+        image: "https://example.com/images/destiny.jpg"
+      },
+      {
+        name: "Bailey",
+        species: "Beluga Whale",
+        length: 14,
+        location: "Marine Life Institute",
+        diet: "Carnivore",
+        image: "https://example.com/images/bailey.jpg"
+      }
     ],
     tips: [
         {
